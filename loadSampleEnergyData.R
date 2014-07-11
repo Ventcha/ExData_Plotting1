d49f0c057138d3f5349665bf3ab523053f104dd9
Rupert Musgrove <rupert.musgrove@ventcha.co.uk>
2014-07-11 16:29:48 +0100

    Updated common function, and recreation of png

--- loadSampleEnergyData.R
+++ loadSampleEnergyData.R
@@ -24,8 +24,8 @@ loadSampleEnergyData <- function() {
  
   # not previously loaded, so will read the file
     message("Load data")
-    fulldata = read.table("../household_power_consumption.txt", sep=";",header = T,stringsAsFactors = F)
-    #attempted to read in with definition of colClasses, but this threw up an unresolvable error, so resort to a more stepwise approach
+    fulldata = read.table("../household_power_consumption.txt", sep=";",header = T,stringsAsFactors = F,
+            na.strings = "?", colClasses=c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
     #create a composite datetime for later conversion: this is still a character
     fulldata$DT = paste(fulldata$Date,fulldata$Time)
     #convert to a date, for the filtering
@@ -35,11 +35,6 @@ loadSampleEnergyData <- function() {
     sample2 = fulldata[fulldata$Date == "2007-02-01" | fulldata$Date == "2007-02-02",]
     #head(sample2) #look at first few records
     rm(fulldata) #remove the original data to clear out memory
-    #convert the columns
-    sample2$Global_active_power = as.numeric(sample2$Global_active_power)
-    sample2$Global_reactive_power = as.numeric(sample2$Global_reactive_power)
-    sample2$Voltage = as.numeric(sample2$Voltage)
-    sample2$Global_intensity = as.numeric(sample2$Global_intensity)
     sample2$DateTime = strptime(sample2$DT, format = "%d/%m/%Y %H:%M:%S")
     #summary(sample2) #examine data
     stored <<- sample2
